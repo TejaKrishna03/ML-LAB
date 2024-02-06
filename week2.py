af5ba1429b7d2ce25bb7a2786478d00668782a10
TejaKrishna03 <157619254+TejaKrishna03@users.noreply.github.com>
2024-02-06 23:41:09 +0530

    Add files via upload

--- week2.py
+++ week2.py
@@ -14,74 +14,74 @@ plt.show()
 
 #
 
-# x =  [1, 2, 3, 4]
-# y1 = [2, 3, 5, 7]
-# y2 = [1, 4, 9, 16]
-# y3 = [3, 6, 9, 12]
+x =  [1, 2, 3, 4]
+y1 = [2, 3, 5, 7]
+y2 = [1, 4, 9, 16]
+y3 = [3, 6, 9, 12]
 
-# plt.plot(x,y1,color='red',label='line1')
-# plt.plot(x,y2,color='blue',label='line2')
-# plt.plot(x,y3,color='green',label="line3")
-# plt.legend()
-# plt.show()   
-
-
-#
-
-# x = np.random.randint(low=1,high=30,size=10)
-# y = np.random.randint(low=1,high=20,size=10)
-
-# plt.scatter(x,y,marker="+")
-# plt.show()
-
-#
-
-# df = pd.read_csv("cereal.csv")
-
-# plt.hist(df.calories)
-# plt.show()
-
-#
-
-# df = pd.read_csv("cereal.csv")
-
-# df = df.head(5)
-# plt.figure(figsize=(8, 5))
-# plt.bar(df.name,df.calories)
-# plt.show()
+plt.plot(x,y1,color='red',label='line1')
+plt.plot(x,y2,color='blue',label='line2')
+plt.plot(x,y3,color='green',label="line3")
+plt.legend()
+plt.show()   
 
 
 #
 
-# categories = ['Apple', 'MI', 'Samsung', 'Oneplus', 'Iqoo', 'poco']
-# expenses = [1500, 300, 800, 600, 400, 200]
+x = np.random.randint(low=1,high=30,size=10)
+y = np.random.randint(low=1,high=20,size=10)
 
-# plt.figure(figsize=(8,5))
-# plt.title("Expense")
-# print(plt.pie(expenses, labels=categories))
-# plt.show()
+plt.scatter(x,y,marker="+")
+plt.show()
+
+#
+
+df = pd.read_csv("cereal.csv")
+
+plt.hist(df.calories)
+plt.show()
+
+#
+
+df = pd.read_csv("cereal.csv")
+
+df = df.head(5)
+plt.figure(figsize=(8, 5))
+plt.bar(df.name,df.calories)
+plt.show()
 
 
 #
 
-# x_positions = [1, 2, 3, 4]
-# y_positions = [5, 4, 3, 2]
-# quantities = [10, 20, 30, 40]
+categories = ['Apple', 'MI', 'Samsung', 'Oneplus', 'Iqoo', 'poco']
+expenses = [1500, 300, 800, 600, 400, 200]
 
-# plt.figure(figsize=(8, 6))
-# plt.scatter(x_positions, y_positions, s=quantities, alpha=0.5,marker="+",c='red')
+plt.figure(figsize=(8,5))
+plt.title("Expense")
+print(plt.pie(expenses, labels=categories))
+plt.show()
 
-# plt.title('Quantities with X and Y Positions')
-# plt.xlabel('X Position')
-# plt.ylabel('Y Position')
-# plt.show()
 
 #
 
-# data = np.random.normal(170, 10, 1000)
+x_positions = [1, 2, 3, 4]
+y_positions = [5, 4, 3, 2]
+quantities = [10, 20, 30, 40]
 
-# plt.hist(data, bins=30, color='skyblue', edgecolor='black')
-# plt.xlabel('Values')
-# plt.ylabel('Frequency')
-# plt.title('Basic Histogram')
-# plt.show()
+plt.figure(figsize=(8, 6))
+plt.scatter(x_positions, y_positions, s=quantities, alpha=0.5,marker="+",c='red')
+
+plt.title('Quantities with X and Y Positions')
+plt.xlabel('X Position')
+plt.ylabel('Y Position')
+plt.show()
+
+#
+
+data = np.random.normal(170, 10, 1000)
+
+plt.hist(data, bins=30, color='skyblue', edgecolor='black')
+plt.xlabel('Values')
+plt.ylabel('Frequency')
+plt.title('Basic Histogram')
+plt.show()
